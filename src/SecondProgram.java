ce76c4082734ad74c44088caa76d40aee8f90dc5
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-05 10:25:16 +0530

    SecondProgram

---
+++ src/SecondProgram.java
@@ -0,0 +1,13 @@
+public class SecondProgram {
+
+    public static void main(String[] args){
+        int a=100;
+        int b=200;
+        int c= a+b;
+        int d= a-b;
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+
+}
