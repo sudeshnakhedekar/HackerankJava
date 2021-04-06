0049e5247f4f9e755a04801cb527138338849398
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-06 09:36:39 +0530

    PrintIntNString

---
+++ src/IntScanner.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class IntScanner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Print integer
+        int a = scanner. nextInt();
+        int b = scanner. nextInt();
+        int c = scanner. nextInt();
+
+        //Print string
+//        String a = scanner. next();
+//        String b = scanner. next();
+//        String c = scanner. next();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
